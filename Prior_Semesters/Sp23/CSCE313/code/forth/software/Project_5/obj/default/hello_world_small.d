97c95030583c647de313b895db2d61bf86907457
j-vaught <jvaught@sc.edu>
2026-01-27 15:15:09 -0500

    refactor: Organize Sp23 course folders with content-type subdirectories matching Fa21 structure

---
+++ Prior_Semesters/Sp23/CSCE313/code/forth/software/Project_5/obj/default/hello_world_small.d
@@ -0,0 +1,81 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../Project_5_bsp//HAL/inc/sys/alt_stdio.h \
+ ../Project_5_bsp//HAL/inc/alt_types.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/math.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/reent.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/_ansi.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/newlib.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/config.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/machine/ieeefp.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/features.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/_types.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/machine/_types.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/machine/_default_types.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/lock.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/_ansi.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/string.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/cdefs.h \
+ ../Project_5_bsp//newlib/nios2-elf/include/sys/string.h \
+ ../Project_5_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../Project_5_bsp//HAL/inc/sys/alt_dev.h ../Project_5_bsp/system.h \
+ ../Project_5_bsp/linker.h ../Project_5_bsp//HAL/inc/sys/alt_llist.h \
+ ../Project_5_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Project_5_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Project_5_bsp//HAL/inc/sys/alt_llist.h \
+ ../Project_5_bsp//HAL/inc/priv/alt_alarm.h myfile.h myfile.c
+
+../Project_5_bsp//HAL/inc/sys/alt_stdio.h:
+
+../Project_5_bsp//HAL/inc/alt_types.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/math.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/reent.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/_ansi.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/newlib.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/config.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/machine/ieeefp.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/features.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/_types.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/machine/_types.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/machine/_default_types.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/lock.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/_ansi.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/string.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/cdefs.h:
+
+../Project_5_bsp//newlib/nios2-elf/include/sys/string.h:
+
+../Project_5_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../Project_5_bsp//HAL/inc/sys/alt_dev.h:
+
+../Project_5_bsp/system.h:
+
+../Project_5_bsp/linker.h:
+
+../Project_5_bsp//HAL/inc/sys/alt_llist.h:
+
+../Project_5_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Project_5_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Project_5_bsp//HAL/inc/sys/alt_llist.h:
+
+../Project_5_bsp//HAL/inc/priv/alt_alarm.h:
+
+myfile.h:
+
+myfile.c:
