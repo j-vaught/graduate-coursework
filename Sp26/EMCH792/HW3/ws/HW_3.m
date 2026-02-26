045e7ce594c0e1c8d6b5e58584e52330922b811a
j-vaught <jvaught@sc.edu>
2026-02-25 22:17:47 -0500

    chore: checkpoint latest local deletions and generated artifacts

---
+++ Sp26/EMCH792/HW3/ws/HW_3.m
@@ -0,0 +1,32 @@
+%% Clear and close everything
+clear; close all; clc;
+
+%% 0. Start by entering your last name
+lastName = "your last name here";
+display("EMCH-792 HW2, " + lastName)
+
+%% B1) Exercise 2.15, page 77
+% Generate 50 independent random variables, find mean, std and plot
+% histogram
+
+% Generate 500 independent random variables, find mean, std and plot
+% histogram
+
+% Generate 5000 independent random variables, find mean, std and plot
+% histogram
+
+%% B2) Noise Covariance matrix calculation
+% Define A, B_u, B_w, S_w, dt1 and dt2
+
+% a. Use the Z block matrix to calculate the exact and approximate Sigma_w
+
+display("Write what you noticed here")
+
+% b. Using the Z block matrix, find Ad in each case. Verify your results
+%    with the c2d() function
+
+% c. Compare predictions of steady-space performance: Using the dlyap() and
+%    lyap(), find Sigma_x_ss in both continuous and discrete systems and
+%    compare them
+
+display("Write what you noticed here")
